4f16b325d74df4487248e62dd72dace32d4d78fe
giovanniurbano <urbagio@gmail.com>
2021-06-11 09:52:56 +0200

    punto 1c

--- src/main/java/it/polito/tdp/imdb/model/Model.java
+++ src/main/java/it/polito/tdp/imdb/model/Model.java
@@ -1,6 +1,8 @@
 package it.polito.tdp.imdb.model;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.jgrapht.Graph;
 import org.jgrapht.Graphs;
@@ -11,6 +13,7 @@ import it.polito.tdp.imdb.db.ImdbDAO;
 
 public class Model {
 	private ImdbDAO dao;
+	private Map<Integer, Actor> idMap;
 	private Graph<Actor, DefaultWeightedEdge> grafo;
 	
 	public Model() {
@@ -26,6 +29,16 @@ public class Model {
 		
 		//vertici
 		Graphs.addAllVertices(this.grafo, this.dao.getActorsByGenre(genere));
+		this.idMap = new HashMap<Integer, Actor>();
+		for(Actor a : this.grafo.vertexSet()) {
+			this.idMap.put(a.getId(), a);
+		}
+		
+		//archi
+		List<Adiacenza> archi = this.dao.getAdiacenze(genere, idMap);
+		for(Adiacenza arco : archi) {
+			Graphs.addEdge(this.grafo, arco.getA1(), arco.getA2(), arco.getPeso());
+		}
 		
 		return String.format("Grafo creato con %d vertici e %d archi\n", this.grafo.vertexSet().size(), this.grafo.edgeSet().size());
 	}
